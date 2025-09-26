755c56101be29f760818ab752b1282d8bb2abbf7
pyaestk <pyaesonethantkyaw206@gmail.com>
2025-09-26 11:49:48 +0700

    setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <h1 className="">Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
